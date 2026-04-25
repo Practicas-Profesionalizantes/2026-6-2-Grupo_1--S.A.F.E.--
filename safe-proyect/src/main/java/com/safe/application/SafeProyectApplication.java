f1ae198890a84e58bf9070d08ec1d0e61075350f
Vigo-Santiago <128174652+Santyiy@users.noreply.github.com>
2026-04-24 21:12:39 -0300

    Update

---
+++ safe-proyect/src/main/java/com/safe/application/SafeProyectApplication.java
@@ -0,0 +1,13 @@
+package com.safe.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeProyectApplication.class, args);
+	}
+
+}
