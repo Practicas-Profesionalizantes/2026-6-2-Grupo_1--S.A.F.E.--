02e5c7ca36f60c12cb9d6b2327a0e0ac1bde1331
leonel pedraza <leonel.pedraza.et32@gmail.com>
2026-06-26 16:20:07 -0300

    Delete carpeta backen safe 2.0 (para fijarse)/2026-6-2-Grupo_1--S.A.F.E.---Backend-SAFE directory

--- carpeta backen safe 2.0 (para fijarse)/2026-6-2-Grupo_1--S.A.F.E.---Backend-SAFE/safe-proyect/src/main/java/com/safe/repository/EvaluacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.safe.repository;
-
-import com.safe.model.Evaluacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EvaluacionRepository extends JpaRepository<Evaluacion, Integer> {
-}
